f632b372f8ac626f1769e5148f1aa67db147909c
tomcat <tommypo26@gmail.com>
2022-02-26 21:33:10 +0300

    upd

--- dist/token/model.constant.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.TOKEN_MODEL = void 0;
-exports.TOKEN_MODEL = 'Token';
-//# sourceMappingURL=model.constant.js.map
--- dist/user/model.constant.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.USER_MODEL = void 0;
-exports.USER_MODEL = `User`;
-//# sourceMappingURL=model.constant.js.map
